8f885b3c0419ce2799967dbf5e88eaeba2871ef8
Mustafa <mmhabeeb20@gmail.com>
2019-10-08 19:26:50 -0500

    Update now

--- src/main/java/BaccaratGameLogic.java
+++ src/main/java/BaccaratGameLogic.java
@@ -22,12 +22,22 @@ import java.util.ArrayList;
             look at PDF
  */
 
-public class BaccaratGameLogic {
+    public class BaccaratGameLogic {
+
     //The method whoWon will evaluate two hands at the end of the game and return a string
     //depending on the winner: “Player”, “Banker”, “Draw”.
-    //public String whoWon(ArrayList<Card> hand1, ArrayList<Card> hand2){
+    public String whoWon(ArrayList<Card> hand1, ArrayList<Card> hand2){
 
-    //}
+        if(handTotal(hand1) > handTotal(hand2)){
+            return "Player";
+        }
+        else if (handTotal(hand1) < handTotal(hand2)){
+            return "Banker";
+        }
+        else{
+            return "Draw";
+        }
+    }
 
     //The method handTotal will take a
     //hand and return how many points that hand is worth.
@@ -45,9 +55,53 @@ public class BaccaratGameLogic {
 
    //The methods evaluateBankerDraw and evaluatePlayerDraw will return true if
    //either one should be dealt a third card otherwise return false.
-   //public boolean evaluateBankerDraw(ArrayList<Card> hand, Card playerCard){
+   public boolean evaluateBankerDraw(ArrayList<Card> hand, Card playerCard){
+       //if the bankers first two cards total 7 or more, no more cards are dealt.
+       if(handTotal(hand) >= 7){
+           return false;
+       }
+       //If The Banker’s cards total 2 or less, The Banker gets one additional card.
+       if(handTotal(hand) <= 2){
+           return true;
+       }
+       else{
+           //If The Bankers first two cards total 3, 4, 5, or 6, it depends on if The Player drew
+           // another card and if so, the value of that card to determine if The Banker receives another card. look at PDF
 
-   //}
+           //if the Player does not draw a card,
+           if(playerCard == null){
+               if(handTotal(hand) <= 5){
+                   return true;
+               }
+           }
+
+           //if the bankers point value is 3 && playerCard is not 8
+           else if((handTotal(hand) == 3) && (playerCard.value != 8)){
+               return true;
+           }
+
+           //if the bankers point value is 4 && playerCard that is not 0, 1, 8 or 9
+           else if((handTotal(hand) == 4) && (playerCard.value != 0 || playerCard.value != 1 || playerCard.value != 8 || playerCard.value != 9 )){
+               return true;
+           }
+
+           //if the bankers point value is 5 && the playerCard either DNE (null or > 0), or values 4-7 including 4 and 7.
+           //NOTE**cant do playerCard.value == null because not an int
+           else if((handTotal(hand) == 5) && (playerCard.value > 0 || (playerCard.value <=4 && playerCard.value >= 7))){
+               return true;
+           }
+
+           //if the bankers point value is 6  && playerCard == 6 || 7
+           else if((handTotal(hand) == 6) && (playerCard.value == 6 || playerCard.value == 7)) {
+               return true;
+           }
+           else{
+               return false;
+           }
+       }
+       return false;
+   }//END of EBD
+
 
     public boolean evaluatePlayerDraw(ArrayList<Card> hand){
        //if hand totals to 5 or less, The Player gets one more card.
