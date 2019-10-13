f116d752d617ba4c8a93b3b52ad3c46bee105c9d
sraza1999 <sraza1999@gmail.com>
2019-10-13 10:10:24 -0500

    Connected main class with BaccaratDealer
    Used the methods in the BaccaratDealer class and now two random cards are given to the banker and two random cards are given to the player

--- src/main/java/BaccaratGame.java
+++ src/main/java/BaccaratGame.java
@@ -1,4 +1,4 @@
-import java.util.ArrayList;
+import java.util.*;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -9,36 +9,39 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
-import javafx.scene.control.MenuBar;
 import javafx.scene.control.TextField;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 
 public class BaccaratGame extends Application {
-
-	/*// add data members
-	ArrayList<Card> playerHand
-	ArrayList<Card> bankerHand
-	BaccaratDealer theDealer
-	BaccaratGameLogic gameLogic
-	double currentBet
-	double totalWinnings
-
-	*/
-
-
+	
+	ArrayList<Card> playerHand;
+	ArrayList<Card> bankerHand;
+	BaccaratDealer theDealer;
+	BaccaratGameLogic gameLogic;
+	double currentBet;
+	double totalWinnings;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		launch(args);
 	}
 
+	//feel free to remove the starter code from this method
+	@Override
 	public void start(Stage stage) throws Exception {
 		
 		
-		  //Instantiating the BorderPane class  
+		  //Instantiating the BorderPane along with the other objects
 	      BorderPane bPane = new BorderPane(); 
-
+	      theDealer = new BaccaratDealer();
+	      playerHand = new ArrayList<>();
+	      bankerHand = new ArrayList<>();
+	      
+	      VBox vBoxRight = new VBox(); // this vertical box will hold the cards in the right box
+	      VBox vBoxLeft = new VBox();
+	      
 	      // All of the nodes that are in the bottom pane
 	      Button startButton = new Button("Start the Round!");
 	      TextField amtToBid = new TextField();
@@ -47,19 +50,28 @@ public class BaccaratGame extends Application {
 	      Button bankerButton = new Button("Banker");
 	      Button drawButton = new Button("Draw");
 	      
-	      // Eventually will need to have all the cards in a some sort of String array so testing it rn
-	      String cards[] = {
-	    		"2C.png", "2D.png","2H.png"
-	      };
+	      // create the dealer object and create the deck of Cards
+	      theDealer.generateDeck();
+	      theDealer.shuffleDeck();
 	      
+	      // Give both the player and the banker their hands
+	      playerHand = theDealer.dealHand();
+	      bankerHand = theDealer.dealHand();
 	      
-	      Image bankerCard1 = new Image(cards[0]);
+	      // Connect the banker's and player's respective cards to the image of that card
+	      Image bankerCard1 = new Image(bankerHand.get(0).cardImageName);
 		  ImageView bCard1 = new ImageView(bankerCard1);
 		  
-		  Image bankerCard2 = new Image(cards[1]);
+		  Image bankerCard2 = new Image(bankerHand.get(1).cardImageName);
 		  ImageView bCard2 = new ImageView(bankerCard2);
 		  
-	      VBox vBoxRight = new VBox(); // this vertical box will hold the cards in the right box
+		  Image playerCard1 = new Image(playerHand.get(0).cardImageName);
+		  ImageView pCard1 = new ImageView(playerCard1);
+		  
+		  Image playerCard2 = new Image(playerHand.get(1).cardImageName);
+		  ImageView pCard2 = new ImageView(playerCard2);
+		  
+	      
 
 	      // Set all the buttons to NOT visible until the start button is pressed 
 	      setBottomPaneNodesToNotVisible( amtToBid, bidButton, playerButton, bankerButton, drawButton);
@@ -67,7 +79,7 @@ public class BaccaratGame extends Application {
 	      // dont allow the bid button to be pressed until an option is selected
 	      bidButton.setDisable(true);
 	      
-	      // When the start button is selected, set them all to visible
+	      // When the start button is selected, set them all to visible (will also need to 
 	      startButton.setOnAction(new EventHandler <ActionEvent> () {
 	    	  public void handle(ActionEvent action) {
 	    	      setBottomPaneNodesToVisible( amtToBid, bidButton, playerButton, bankerButton, drawButton);
@@ -107,13 +119,23 @@ public class BaccaratGame extends Application {
 	      // Hide the cards until the Bid Button is pressed, then show them
 	      bidButton.setOnAction(new EventHandler<ActionEvent>() {
 	    	  public void handle(ActionEvent action) {
-	    		  vBoxRight.getChildren().add(bCard1);	    		  
-	    		  vBoxRight.getChildren().add(bCard2);
+	    		  if(isNumeric(amtToBid.getText())) {
+	    			  vBoxLeft.getChildren().add(pCard1);	    		  
+		    		  vBoxLeft.getChildren().add(pCard2);
+		    		  
+		    		  vBoxRight.getChildren().add(bCard1);
+		    		  vBoxRight.getChildren().add(bCard2);
+		    		  
+		    		  }
+	    		  else {
+	    			  amtToBid.setText("Input a Number!");
+	    		  }
+	    		  
 
 	    	  }
 	      });
 		  
-	      
+	      bPane.setLeft(vBoxLeft);
 	      bPane.setRight(vBoxRight); // Banker Hand
 	      bPane.setBottom(hBox); // Game Controls
 	      
@@ -154,5 +176,15 @@ public class BaccaratGame extends Application {
 	      bankerButton.setVisible(true);
 	      drawButton.setVisible(true);
 	}
-
+	
+	// Should prob change before/just make a different method before submitting
+	public static boolean isNumeric(String strNum) {
+	    try {
+	        Integer d = Integer.parseInt(strNum);
+	    } catch (NumberFormatException | NullPointerException nfe) {
+	        return false;
+	    }
+	    return true;
+	}
+	
 }
